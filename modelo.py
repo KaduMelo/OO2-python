bd0e6408aa5710f42ddb1058a91d0cb30b296909
Carlos Eduardo <carlos.silva@gt4w.com.br>
2020-09-23 05:45:37 -0300

    reafct: Improving code for OO

--- modelo.py
+++ modelo.py
@@ -19,15 +19,24 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+    def __str__(self):
+        return f'Nome: {self._nome} - Likes: {self.likes}'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
+
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -40,3 +49,8 @@ atlanta.dar_likes()
 
 print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
 print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    print(programa)
